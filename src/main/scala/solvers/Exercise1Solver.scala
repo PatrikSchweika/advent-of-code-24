5454d4931e5e852ecc3cac2eaa94cab53b3c966e
Patrik Schweika <patrik.schweika@gmail.com>
2024-12-01 15:22:31 +0100

    exercise 1

--- src/main/scala/solvers/Exercise1Solver.scala
+++ src/main/scala/solvers/Exercise1Solver.scala
@@ -1,6 +1,26 @@
 package solvers
 
+import scala.collection.mutable
+
 object Exercise1Solver extends Solver {
-  override def solve(lines: Seq[String]): Int = 42
+  override def solve(lines: Seq[String]): Int = {
+    val leftNumbers = mutable.PriorityQueue[Int]()
+    val rightNumbers = mutable.PriorityQueue[Int]()
+
+    for (line <- lines) {
+      val nums = line.split(" +")
+
+      leftNumbers.addOne(nums(0).toInt)
+      rightNumbers.addOne(nums(1).toInt)
+    }
+
+    val sortedLeft = leftNumbers.dequeueAll
+    val sortedRight = rightNumbers.dequeueAll
+
+    sortedLeft
+      .zip(sortedRight)
+      .map((left, right) => math.abs(left - right))
+      .sum()
+  }
 }
   
